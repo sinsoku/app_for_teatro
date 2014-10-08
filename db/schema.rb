044ac772c21f709da2fb66b0710052da1fca6ee5
sinsoku <sinsoku.listy@gmail.com>
2014-10-09 00:34:28 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140927151507) do
+ActiveRecord::Schema.define(version: 20141008153303) do
+
+  create_table "books", force: true do |t|
+    t.string   "title"
+    t.string   "author"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
